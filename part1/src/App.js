bd0f73a3e4ae188a9ac0d6bd93170dea04f2002f
SAMEER SHRESTHA <Nero0293@outlook.com>
2023-03-03 00:19:23 +0545

    1.3,1.4,1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,20 +1,28 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7 
-  const part3 = 'State of component'
-  const exercises3 = 14
+  const course = {
+    name : 'Half Stack application development',
+      parts : [
+    {
+    name : 'Fundamentals of React',
+    exercises1 : 10
+    },
+    {
+    name : 'Using props to pass data',
+    exercises2 : 7 
+    },
+    {
+    name : 'State of component',
+    exercises3 : 14
+    }
+  ]
+}
 
   return(
     <div>
       <Header course={course}/>
-      <Content part = {part1} exercise = {exercises1}/>
-      <Content part = {part2} exercise = {exercises2}/>
-      <Content part = {part3} exercise = {exercises3}/>
-      <Total exercise = {[exercises1, exercises2, exercises3]}/>
-      
+      <Content part = {parts}/>
+      <Total exercise = {parts}/>
+    
     </div>
   )
 }
